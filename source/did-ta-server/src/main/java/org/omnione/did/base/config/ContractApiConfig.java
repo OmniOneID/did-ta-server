da5f8ff82f21c74a191bc6e687bbc67aafdddd62
YoonGyu Lee <yklee0911@raoncorp.com>
2025-06-05 16:01:34 +0900

    feat: fixed typo in application-lls filename (should be lss), applied lss profile rules

--- source/did-ta-server/src/main/java/org/omnione/did/base/config/ContractApiConfig.java
+++ source/did-ta-server/src/main/java/org/omnione/did/base/config/ContractApiConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!sample")
+@Profile("!sample & !lss")
 public class ContractApiConfig {
     @Bean
     public ContractApi contractApi(BlockchainProperty blockchainProperty) {
